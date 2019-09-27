1849055942d9cf509dc45feb91384a6496d7ea7e
Tomasz Klocek <tomek@klocek.info>
2019-09-27 12:12:57 +0200

    Completed interface building

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Tomek Klocek on 2019-09-27.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
