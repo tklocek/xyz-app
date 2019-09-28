789e40967596c84c48a8a010f24de84bb7d0d10e
Tomasz Klocek <tomek@klocek.info>
2019-09-28 08:35:53 +0200

    Something broken

--- xyz-machine/xyz-machine/ViewController.swift
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
@@ -14,12 +15,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager: CMMotionManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("test")
+        zLabel.text = "Stage 0"
+      //  motionManager = CMMotionManager()
+      //  motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+        }
+    
+
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        xLabel.text = "Stage 1"
+        guard let  accelerometerData = data else { return }
+        yLabel.text = "Stage 2"
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "X: \(x)"
+        yLabel.text = "Y: \(y)"
+        zLabel.text = "Z: \(z)"
+        
     }
 
-
+    
+    
 }
 
